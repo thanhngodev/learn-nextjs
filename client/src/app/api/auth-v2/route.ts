73b27aad9b892938b89128d9151a80ee3f5bd705
thanhngodev <thanhngodev@gmail.com>
2024-06-01 19:13:30 +0700

    check

---
+++ client/src/app/api/auth-v2/route.ts
@@ -0,0 +1,25 @@
+export async function POST(request: Request) {
+  const body = await request.json();
+  const sessionToken = body.sessionToken as string;
+  const expiresAt = body.expiresAt as string;
+  console.log(body);
+
+
+  return Response.json({body})
+  
+  // if (!sessionToken) {
+  //   return Response.json(
+  //     { message: "Không nhận được session token" },
+  //     {
+  //       status: 400,
+  //     }
+  //   );
+  // }
+  // const expiresDate = new Date(expiresAt).toUTCString();
+  // return Response.json(body, {
+  //   status: 200,
+  //   headers: {
+  //     "Set-Cookie": `sessionToken=${sessionToken}; Path=/; HttpOnly; Expires=${expiresDate}; SameSite=Lax; Secure`,
+  //   },
+  // });
+}
